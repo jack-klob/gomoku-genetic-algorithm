573d67103766cce4ebe411078ced220b182d8e92
CK <chance.kane99@gmail.com>
2025-07-17 22:23:26 -0700

    Revised threat analysis from convolution approach to direction-wise tallying at relevant points

--- base-brain/gomokuAgentTemplate.py
+++ base-brain/gomokuAgentTemplate.py
@@ -23,45 +23,110 @@ verticalKernel[:,4] = 1
 verticalKernel[4,4] = 0
 horizontalKernel = np.rot90(verticalKernel)
 kernels = [diagKernel, antiDiagKernel, verticalKernel, horizontalKernel]
+relevanceKernal = np.ones((9,9), dtype=np.int8) #used to track relevance of a point
+directions = [[0,1],[1,1],[1,0]]
 #possible alternatives for kernels: 8 compass directions? twice as many ops, but better for threats accurately
 
+def clamp(value, min_val, max_val):
+    """Clamps a single numerical value within a specified range."""
+    return max(min(value, max_val), min_val)
 
-def convolved_line_scores(board, player=1):
-    # Player, opponent, and empty masks
-    player_mask = (board == player)*1
-    opponent_mask = (board == -player)*1
-    empty_mask = (board == 0)*1
+#Generate and return a board mask indicating which positions are within relevance range of another piece
+def identify_relevant_positions(board):
+  relevantMask = np.zeros_like(board, dtype=int)  #create blank mask
+  convolved = signal.convolve2d(board, relevanceKernal, mode='same', boundary='fill', fillvalue=0) #convolve board with 9*9 of 1s to render any point within 5 squares of a piece nonzero
+  relevantMask = (convolved != 0)*1    #update mask to show locations within 5 of another piece
+  return relevantMask
 
-    score_map = np.zeros_like(board, dtype=int)
+def calculate_value_at_point(board, x, y, maxLength = 4,player = 1):
+  value = 0 
+  lookup = np.array(genome[:-1])
+  for direction in directions:
+    i,j = x,y
+    tilesInARow = 1; 
+    lengthPos,lengthNeg = 0,0
+    blocked = False
 
-    for kernel in kernels:
-        # Convolve player presence
-        player_conv = signal.convolve2d(player_mask, kernel, mode='same', boundary='fill', fillvalue=0)
-        # Convolve opponent presence to mask broken lines
-        opponent_conv = signal.convolve2d(opponent_mask, kernel, mode='same', boundary='fill', fillvalue=0)
+    for k in range(1,maxLength):  #crawl direction out to max length    
+      i += direction[0]
+      j += direction[1]
+      if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+        lengthPos = k-1
+        blocked = True
+        break
+      if board[i][j] == 0:
+        continue
+      elif board[i][j] == player:  #if new piece of 
+        tilesInARow+=1
+      elif board[i][j] == -player: #if there's an enemy piece, threat cannot continue in this direction
+        lengthPos = k-1
+        blocked = True
+        break
+    i,j = x,y
+    for k in range(1,maxLength):  #crawl direction out to max length    
+      i -= direction[0]
+      j -= direction[1]
+      if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+        lengthNeg = k-1
+        blocked = True
+        break
+      if board[i][j] == 0:
+        continue
+      elif board[i][j] == player:  #if new piece of 
+        tilesInARow+=1
+      elif board[i][j] == -player: #if there's an enemy piece, threat cannot continue in this direction
+        lengthNeg = k-1
+        blocked = True
+        break
+    if lengthNeg !=0 and lengthPos != 0: #if continuous length is bounded on both ends
+      maxLengthAlongAxis = lengthNeg + lengthPos + 1
+      #if the max length of contiguous spaces containing this point between obstructions can't possibly generate a winning threat
+      if maxLengthAlongAxis < 5: 
+        break
+    if tilesInARow >= 5:  
+       return np.inf #if this point is part of a winning threat, return infinity
+    if blocked:
+      value += lookup[clamp(2*(tilesInARow)-2,0, len(lookup)-1)]  #if the threat is blocked on one side, use the lookup table to get the value of the threat
+    else:
+      value += lookup[clamp(2*(tilesInARow)-1,0, len(lookup)-1)]
+  return value
 
-        # Score only at empty locations, and only if no opponent pieces block the line
-        valid = (empty_mask == 1) & (opponent_conv == 0)
-        score_map += np.where(valid, player_conv, 0)*1
+# def convolved_line_scores(board, player=1):
+#     # Player, opponent, and empty masks
+#     player_mask = (board == player)*1
+#     opponent_mask = (board == -player)*1
+#     empty_mask = (board == 0)*1
 
-    return score_map
+#     score_map = np.zeros_like(board, dtype=int)
 
-def generate_point_valuations():
-    npBoard = np.array(board, dtype=int)
-    npBoard[npBoard ==2] = -1
-    offensiveScoreMap = convolved_line_scores(npBoard, player=1)    
-    defensiveScoreMap = convolved_line_scores(npBoard, player=-1)
-    lookup = np.array(genome[:-1])
-    offensiveScoreMap = lookup[offensiveScoreMap]
-    defensiveScoreMap = lookup[defensiveScoreMap]
-    # print("Defensive Score Map: ")
-    # for row in defensiveScoreMap:
-    #     print(row)
-    overallPointValues = (genome[-1] * offensiveScoreMap) + (1-genome[-1]) * defensiveScoreMap
-    # print("Overall Points Values: ")
-    # for row in overallPointValues:
-    #     print(row)
-    return overallPointValues
+#     for kernel in kernels:
+#         # Convolve player presence
+#         player_conv = signal.convolve2d(player_mask, kernel, mode='same', boundary='fill', fillvalue=0)
+#         # Convolve opponent presence to mask broken lines
+#         opponent_conv = signal.convolve2d(opponent_mask, kernel, mode='same', boundary='fill', fillvalue=0)
+
+#         # Score only at empty locations, and only if no opponent pieces block the line
+#         valid = (empty_mask == 1) & (opponent_conv == 0)
+#         score_map += np.where(valid, player_conv, 0)*1
+
+#     return score_map
+
+# def generate_point_valuations():
+#     npBoard = np.array(board, dtype=int)
+#     npBoard[npBoard ==2] = -1
+#     offensiveScoreMap = convolved_line_scores(npBoard, player=1)    
+#     defensiveScoreMap = convolved_line_scores(npBoard, player=-1)
+#     lookup = np.array(genome[:-1])
+#     offensiveScoreMap = lookup[offensiveScoreMap]
+#     defensiveScoreMap = lookup[defensiveScoreMap]
+#     # print("Defensive Score Map: ")
+#     # for row in defensiveScoreMap:
+#     #     print(row)
+#     overallPointValues = (genome[-1] * offensiveScoreMap) + (1-genome[-1]) * defensiveScoreMap
+#     # print("Overall Points Values: ")
+#     # for row in overallPointValues:
+#     #     print(row)
+#     return overallPointValues
 
 def brain_init():
     if state.width < 5 or state.height < 5:
@@ -120,12 +185,33 @@ def brain_turn():
     #if AI slated for termination, return immediately
     if state.terminate_ai:
         return
-    heatmap = generate_point_valuations()
-    x, y = np.unravel_index(np.argmax(heatmap), heatmap.shape)
-    p = Point(int(x), int(y))
-    if not (is_free(p) and is_valid(p)):
-        pp.pipe_out(f"ERROR my move {p}")
+    npBoard = np.array(board, dtype=int)
+    offensiveScores = np.zeros_like(board, dtype=int)
+    defensiveScores = np.zeros_like(board, dtype=int)
+    totalScores = np.zeros_like(board, dtype=int)
+    relevantPositions = identify_relevant_positions(npBoard)
+    #Replace Gomocup standard format of 2 = opponent piece for computations
+    npBoard[npBoard == 2] = -1
+    validPositions = np.zeros_like(board, dtype=int)
+    validPositions[npBoard==0] = 1
+    for x in range(len(board)):
+        for y in range(len(board[0])):
+            if relevantPositions[x][y] == 1 and validPositions[x][y]==1:    #only calculate scores for valid points within 5 of another piece
+                offensiveScores[x][y] = calculate_value_at_point(npBoard, x, y)
+                defensiveScores[x][y] = calculate_value_at_point(npBoard, x, y, player=-1)
+                totalScores[x][y] = genome[-1]*offensiveScores[x][y] + (1-genome[-1])*defensiveScores[x][y]
+    maxScore = np.max(totalScores)
+    minScore = np.min(totalScores)
+    if maxScore == minScore:  #if all scores are equal, suggest the center of the board
+        pp.suggest(int(state.width // 2), int(state.height // 2))
         return
+    else:
+        # Find the point with the maximum score
+        x, y = np.unravel_index(np.argmax(totalScores), totalScores.shape)
+        p = Point(int(x), int(y))
+        if not (is_free(p) and is_valid(p)):
+            pp.pipe_out(f"ERROR my move {p}")
+            return
     
     # i = 0   # number of iterations to find a free point
     # # find a random free point
