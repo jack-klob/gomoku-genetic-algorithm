99f3c2dbd0f185a2b9f8d21c0c65d0c54838eb6d
Jack Klobchar <jackklobchar@gmail.com>
2025-07-23 20:57:00 -0700

    move agent file into base_brain folder. rename file to snake_case. add requirements

--- base_brain/gomoku_agent_template.py
+++ base_brain/gomoku_agent_template.py
@@ -1,95 +1,108 @@
-import random
 import numpy as np
-from scipy import signal
 import pisqpipe as pp
 from pisqpipe import state
+from scipy import signal
 from structs import Point
 
-pp.infotext = (
-    'name="pbrain-geneticPeabrain", authors="Andrew Petten, Chance Kane, Jack Klobchar, Tim Xu"'
-)
+pp.infotext = 'name="pbrain-geneticPeabrain", authors="Andrew Petten, Chance Kane, Jack Klobchar, Tim Xu"'
 # genome for the genetic algorithm 11 values, 10 arbitrary, one constrained to [0,1]
-# #genome = [zero-closed,zero-open,one-closed,one-open,two-closed,two-open,three-closed,three-open,four-closed,four-open, aggression] 
-genome =  [8, 16, 32, 64, 128, 512, 1000, 2000, 0.5]  # genome for the genetic algorithm
+# #genome = [zero-closed,zero-open,one-closed,one-open,two-closed,two-open,three-closed,three-open,four-closed,four-open, aggression]
+genome = [8, 16, 32, 64, 128, 512, 1000, 2000, 0.5]  # genome for the genetic algorithm
 
 MAX_BOARD = 100
-#0 = empty, 1 = my stone, 2 = opponent's stone, 3 = winning move
+# 0 = empty, 1 = my stone, 2 = opponent's stone, 3 = winning move
 board = [[0 for i in range(MAX_BOARD)] for j in range(MAX_BOARD)]
-diagKernel = np.eye(9, dtype = np.int8)
-diagKernel[4,4] = 0
+diagKernel = np.eye(9, dtype=np.int8)
+diagKernel[4, 4] = 0
 antiDiagKernel = np.rot90(diagKernel)
 verticalKernel = np.zeros_like(antiDiagKernel)
-verticalKernel[:,4] = 1
-verticalKernel[4,4] = 0
+verticalKernel[:, 4] = 1
+verticalKernel[4, 4] = 0
 horizontalKernel = np.rot90(verticalKernel)
 kernels = [diagKernel, antiDiagKernel, verticalKernel, horizontalKernel]
-relevanceKernal = np.ones((9,9), dtype=np.int8) #used to track relevance of a point
-directions = [[0,1],[1,1],[1,0],[1,-1]]
-#possible alternatives for kernels: 8 compass directions? twice as many ops, but better for threats accurately
+relevanceKernal = np.ones((9, 9), dtype=np.int8)  # used to track relevance of a point
+directions = [[0, 1], [1, 1], [1, 0], [1, -1]]
+# possible alternatives for kernels: 8 compass directions? twice as many ops, but better for threats accurately
+
 
 def clamp(value, min_val, max_val):
     """Clamps a single numerical value within a specified range."""
     return max(min(value, max_val), min_val)
 
-#Generate and return a board mask indicating which positions are within relevance range of another piece
+
+# Generate and return a board mask indicating which positions are within relevance range of another piece
 def identify_relevant_positions(board):
-  relevantMask = np.zeros_like(board, dtype=int)  #create blank mask
-  convolved = signal.convolve2d(board, relevanceKernal, mode='same', boundary='fill', fillvalue=0) #convolve board with 9*9 of 1s to render any point within 5 squares of a piece nonzero
-  relevantMask = (convolved != 0)*1    #update mask to show locations within 5 of another piece
-  return relevantMask
+    relevantMask = np.zeros_like(board, dtype=int)  # create blank mask
+    convolved = signal.convolve2d(
+        board, relevanceKernal, mode="same", boundary="fill", fillvalue=0
+    )  # convolve board with 9*9 of 1s to render any point within 5 squares of a piece nonzero
+    relevantMask = (
+        convolved != 0
+    ) * 1  # update mask to show locations within 5 of another piece
+    return relevantMask
 
-def calculate_value_at_point(board, x, y, maxLength = 4,player = 1):
-  value = 0 
-  lookup = np.array(genome[:-1])
-  for direction in directions:
-    i,j = x,y
-    tilesInARow = 1; 
-    lengthPos,lengthNeg = 0,0
-    blocked = False
 
-    for k in range(1,maxLength):  #crawl direction out to max length    
-      i += direction[0]
-      j += direction[1]
-      if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
-        lengthPos = k-1
-        blocked = True
-        break
-      if board[i][j] == 0:
-        continue
-      elif board[i][j] == player:  #if new piece of 
-        tilesInARow+=1
-      elif board[i][j] == -player: #if there's an enemy piece, threat cannot continue in this direction
-        lengthPos = k-1
-        blocked = True
-        break
-    i,j = x,y
-    for k in range(1,maxLength):  #crawl direction out to max length    
-      i -= direction[0]
-      j -= direction[1]
-      if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
-        lengthNeg = k-1
-        blocked = True
-        break
-      if board[i][j] == 0:
-        continue
-      elif board[i][j] == player:  #if new piece of 
-        tilesInARow+=1
-      elif board[i][j] == -player: #if there's an enemy piece, threat cannot continue in this direction
-        lengthNeg = k-1
-        blocked = True
-        break
-    if lengthNeg !=0 and lengthPos != 0: #if continuous length is bounded on both ends
-      maxLengthAlongAxis = lengthNeg + lengthPos + 1
-      #if the max length of contiguous spaces containing this point between obstructions can't possibly generate a winning threat
-      if maxLengthAlongAxis < 5: 
-        break
-    if tilesInARow >= 5:  
-       return np.inf #if this point is part of a winning threat, return infinity
-    if blocked:
-      value += lookup[clamp(2*(tilesInARow)-2,0, len(lookup)-1)]  #if the threat is blocked on one side, use the lookup table to get the value of the threat
-    else:
-      value += lookup[clamp(2*(tilesInARow)-1,0, len(lookup)-1)]
-  return value
+def calculate_value_at_point(board, x, y, maxLength=4, player=1):
+    value = 0
+    lookup = np.array(genome[:-1])
+    for direction in directions:
+        i, j = x, y
+        tilesInARow = 1
+        lengthPos, lengthNeg = 0, 0
+        blocked = False
+
+        for k in range(1, maxLength):  # crawl direction out to max length
+            i += direction[0]
+            j += direction[1]
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+                lengthPos = k - 1
+                blocked = True
+                break
+            if board[i][j] == 0:
+                continue
+            elif board[i][j] == player:  # if new piece of
+                tilesInARow += 1
+            elif (
+                board[i][j] == -player
+            ):  # if there's an enemy piece, threat cannot continue in this direction
+                lengthPos = k - 1
+                blocked = True
+                break
+        i, j = x, y
+        for k in range(1, maxLength):  # crawl direction out to max length
+            i -= direction[0]
+            j -= direction[1]
+            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]):
+                lengthNeg = k - 1
+                blocked = True
+                break
+            if board[i][j] == 0:
+                continue
+            elif board[i][j] == player:  # if new piece of
+                tilesInARow += 1
+            elif (
+                board[i][j] == -player
+            ):  # if there's an enemy piece, threat cannot continue in this direction
+                lengthNeg = k - 1
+                blocked = True
+                break
+        if (
+            lengthNeg != 0 and lengthPos != 0
+        ):  # if continuous length is bounded on both ends
+            maxLengthAlongAxis = lengthNeg + lengthPos + 1
+            # if the max length of contiguous spaces containing this point between obstructions can't possibly generate a winning threat
+            if maxLengthAlongAxis < 5:
+                break
+        if tilesInARow >= 5:
+            return np.inf  # if this point is part of a winning threat, return infinity
+        if blocked:
+            value += lookup[
+                clamp(2 * (tilesInARow) - 2, 0, len(lookup) - 1)
+            ]  # if the threat is blocked on one side, use the lookup table to get the value of the threat
+        else:
+            value += lookup[clamp(2 * (tilesInARow) - 1, 0, len(lookup) - 1)]
+    return value
+
 
 # def convolved_line_scores(board, player=1):
 #     # Player, opponent, and empty masks
@@ -114,7 +127,7 @@ def calculate_value_at_point(board, x, y, maxLength = 4,player = 1):
 # def generate_point_valuations():
 #     npBoard = np.array(board, dtype=int)
 #     npBoard[npBoard ==2] = -1
-#     offensiveScoreMap = convolved_line_scores(npBoard, player=1)    
+#     offensiveScoreMap = convolved_line_scores(npBoard, player=1)
 #     defensiveScoreMap = convolved_line_scores(npBoard, player=-1)
 #     lookup = np.array(genome[:-1])
 #     offensiveScoreMap = lookup[offensiveScoreMap]
@@ -127,6 +140,7 @@ def calculate_value_at_point(board, x, y, maxLength = 4,player = 1):
 #     # for row in overallPointValues:
 #     #     print(row)
 #     return overallPointValues
+
 
 def brain_init():
     if state.width < 5 or state.height < 5:
@@ -175,14 +189,14 @@ def brain_block(p: Point):
 
 
 def brain_takeback(p: Point):
-    if (is_valid(p) and (board[p.x][p.y] != 0)):
+    if is_valid(p) and (board[p.x][p.y] != 0):
         board[p.x][p.y] = 0
         return 0
     return 2
 
 
 def brain_turn():
-    #if AI slated for termination, return immediately
+    # if AI slated for termination, return immediately
     if state.terminate_ai:
         return
     npBoard = np.array(board, dtype=int)
@@ -190,19 +204,26 @@ def brain_turn():
     defensiveScores = np.zeros_like(board, dtype=int)
     totalScores = np.zeros_like(board, dtype=int)
     relevantPositions = identify_relevant_positions(npBoard)
-    #Replace Gomocup standard format of 2 = opponent piece for computations
+    # Replace Gomocup standard format of 2 = opponent piece for computations
     npBoard[npBoard == 2] = -1
     validPositions = np.zeros_like(board, dtype=int)
-    validPositions[npBoard==0] = 1
+    validPositions[npBoard == 0] = 1
     for x in range(len(board)):
         for y in range(len(board[0])):
-            if relevantPositions[x][y] == 1 and validPositions[x][y]==1:    #only calculate scores for valid points within 5 of another piece
+            if (
+                relevantPositions[x][y] == 1 and validPositions[x][y] == 1
+            ):  # only calculate scores for valid points within 5 of another piece
                 offensiveScores[x][y] = calculate_value_at_point(npBoard, x, y)
-                defensiveScores[x][y] = calculate_value_at_point(npBoard, x, y, player=-1)
-                totalScores[x][y] = genome[-1]*offensiveScores[x][y] + (1-genome[-1])*defensiveScores[x][y]
+                defensiveScores[x][y] = calculate_value_at_point(
+                    npBoard, x, y, player=-1
+                )
+                totalScores[x][y] = (
+                    genome[-1] * offensiveScores[x][y]
+                    + (1 - genome[-1]) * defensiveScores[x][y]
+                )
     maxScore = np.max(totalScores)
     minScore = np.min(totalScores)
-    if maxScore == minScore:  #if all scores are equal, suggest the center of the board
+    if maxScore == minScore:  # if all scores are equal, suggest the center of the board
         pp.suggest(int(state.width // 2), int(state.height // 2))
         return
     else:
@@ -212,7 +233,7 @@ def brain_turn():
         if not (is_free(p) and is_valid(p)):
             pp.pipe_out(f"ERROR my move {p}")
             return
-    
+
     # i = 0   # number of iterations to find a free point
     # # find a random free point
     # while True:
